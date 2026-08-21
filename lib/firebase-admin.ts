11583d60e7d15617f4adca381e06817dc5f1cf02
wwwshahrkarapp-hub <www.shahrkar.app@gmail.com>
2026-08-22 01:12:06 +0330

    use firebase admin env key

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,10 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../firebase-admin-key.json";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_ADMIN_KEY || "{}"
+);
 
 console.log("KEY PROJECT:", serviceAccount.project_id);
 
